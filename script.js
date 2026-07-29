fd754e8ad6eb00b6d1fed5b3fce9255d3c028a95
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-29 19:40:37 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("👑 KAYLA MODE pare!");
+
+
+const bouton = document.querySelector("button");
+
+
+bouton.addEventListener("click", function(){
+
+alert("Byenveni nan KAYLA MODE 🛍️ Nouvo koleksyon yo ap vini!");
+
+});
