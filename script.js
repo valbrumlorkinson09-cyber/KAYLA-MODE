62f90bceeb83cb3a3a6551d436a8869a35563675
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-29 20:54:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 console.log("👑 KAYLA MODE pare!");
 
 
-const bouton = document.querySelector("button");
+// RECHÈCH PWODWI
 
-
-bouton.addEventListener("click", function(){
-
-alert("Byenveni nan KAYLA MODE 🛍️ Nouvo koleksyon yo ap vini!");
-
-});
 function searchProduct(){
 
-let search = document.getElementById("searchBox").value.toLowerCase();
+let search = document
+.getElementById("searchBox")
+.value
+.toLowerCase();
+
 
 if(search.includes("rob")){
+
 alert("👗 Rob Elegant disponib - 2500 Gdes");
+
 }
+
 
 else if(search.includes("soulye")){
+
 alert("👟 Soulye Fashion disponib - 3000 Gdes");
+
 }
+
 
 else if(search.includes("sak")){
+
 alert("👜 Sak Fashion disponib - 2000 Gdes");
+
 }
 
+
 else{
+
 alert("❌ Nou pa jwenn pwodwi sa a");
+
 }
 
 }
