af193dcebb5f69a4cf356478715520b6e16a5771
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-30 22:49:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,191 @@
-// KAYLA MODE - Chaje pwodwi yo
+// KAYLA MODE - SCRIPT PRINCIPAL
+
 
 function afficherProduits(){
 
+
     const productList = document.getElementById("productList");
 
+
     if(!productList){
+
         return;
+
     }
+
 
 
     let produits = JSON.parse(localStorage.getItem("produits")) || [];
 
 
+
     productList.innerHTML = "";
+
+
+
+
+    if(produits.length === 0){
+
+
+        productList.innerHTML = `
+
+        <p>
+        🛍️ Pa gen pwodwi pou kounya.
+        </p>
+
+        `;
+
+
+        return;
+
+    }
+
+
+
+
+
 
 
     produits.forEach(function(produit){
 
 
+
         productList.innerHTML += `
+
 
         <div class="product">
 
+
             <img src="${produit.image}" alt="${produit.nom}">
 
-            <h3>${produit.nom}</h3>
+
+
+            <h3>
+            ${produit.nom}
+            </h3>
+
+
 
             <div class="stars">
             ★★★★★
             </div>
 
-            <p>${produit.prix}</p>
+
+
+            <p>
+            ${produit.prix}
+            </p>
+
+
 
 
             <a href="https://wa.me/50955545291" class="buy">
+
             Achte
+
             </a>
 
+
+
         </div>
+
+
 
         `;
 
 
+
     });
 
+
+
 }
+
+
+
+
+
+
+
+function searchProduct(){
+
+
+    const searchBox = document.getElementById("searchBox");
+
+
+    const rech = searchBox.value.toLowerCase();
+
+
+
+    let produits = JSON.parse(localStorage.getItem("produits")) || [];
+
+
+
+    const productList = document.getElementById("productList");
+
+
+
+    productList.innerHTML = "";
+
+
+
+
+    produits.filter(function(produit){
+
+
+        return produit.nom.toLowerCase().includes(rech);
+
+
+    }).forEach(function(produit){
+
+
+
+        productList.innerHTML += `
+
+
+
+        <div class="product">
+
+
+        <img src="${produit.image}">
+
+
+        <h3>${produit.nom}</h3>
+
+
+        <div class="stars">
+        ★★★★★
+        </div>
+
+
+        <p>${produit.prix}</p>
+
+
+
+        <a href="https://wa.me/50955545291" class="buy">
+
+        Achte
+
+        </a>
+
+
+        </div>
+
+
+
+        `;
+
+
+
+    });
+
+
+
+}
+
+
+
+
 
 
 
