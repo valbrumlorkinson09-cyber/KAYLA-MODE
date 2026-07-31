1268a325f18bfdfd300bf64221a5661de4492deb
valbrumlorkinson09-cyber <valbrumlorkinson09@gmail.com>
2026-07-30 22:18:46 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,7 +2,10 @@ function loginAdmin(){
 
     const password = document.getElementById("adminPassword").value;
 
+
     if(password === "KAYLA2026"){
+
+        localStorage.setItem("adminLogin","true");
 
         document.getElementById("loginBox").style.display = "none";
         document.getElementById("adminPanel").style.display = "block";
@@ -17,12 +20,32 @@ function loginAdmin(){
 
 
 
+
+function checkLogin(){
+
+    if(localStorage.getItem("adminLogin") === "true"){
+
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("adminPanel").style.display = "block";
+
+    }
+
+}
+
+
+
+
+
 function logoutAdmin(){
+
+    localStorage.removeItem("adminLogin");
 
     document.getElementById("loginBox").style.display = "block";
     document.getElementById("adminPanel").style.display = "none";
 
 }
+
+
 
 
 
@@ -38,6 +61,7 @@ photoProduit.addEventListener("change", function(){
 
     const file = this.files[0];
 
+
     if(file){
 
         imageProduit = URL.createObjectURL(file);
@@ -51,10 +75,13 @@ photoProduit.addEventListener("change", function(){
 
 
 
+
+
 function ajouterProduit(){
 
     const nom = document.getElementById("nomProduit").value;
     const prix = document.getElementById("prixProduit").value;
+
 
 
     if(nom === "" || prix === "" || imageProduit === ""){
@@ -66,33 +93,47 @@ function ajouterProduit(){
     }
 
 
+
+
     let nouveauProduit = {
 
         nom: nom,
+
         prix: prix + " Gdes",
+
         image: imageProduit
 
     };
 
 
+
+
     let produits = JSON.parse(localStorage.getItem("produits")) || [];
+
 
 
     produits.push(nouveauProduit);
 
 
+
     localStorage.setItem("produits", JSON.stringify(produits));
+
 
 
     afficherProduits();
 
 
+
     alert("Pwodwi ajoute avèk siksè ✅");
 
 
+
     document.getElementById("nomProduit").value = "";
+
     document.getElementById("prixProduit").value = "";
+
     previewPhoto.src = "";
+
     imageProduit = "";
 
 }
@@ -101,37 +142,54 @@ function ajouterProduit(){
 
 
 
+
+
+
 function afficherProduits(){
+
 
     const liste = document.getElementById("listeProduits");
 
+
     let produits = JSON.parse(localStorage.getItem("produits")) || [];
+
 
 
     liste.innerHTML = "<h2>🛍️ Pwodwi yo</h2>";
 
 
 
+
     produits.forEach(function(produit,index){
+
 
 
         liste.innerHTML += `
 
+
         <div class="product-admin">
 
-            <img src="${produit.image}" width="120">
 
-            <h3>${produit.nom}</h3>
-
-            <p>${produit.prix}</p>
+        <img src="${produit.image}" width="120">
 
 
-            <button onclick="supprimerProduit(${index})">
-            🗑️ Efase
-            </button>
+        <h3>${produit.nom}</h3>
+
+
+        <p>${produit.prix}</p>
+
+
+
+        <button onclick="supprimerProduit(${index})">
+
+        🗑️ Efase
+
+        </button>
+
 
 
         </div>
+
 
         `;
 
@@ -144,26 +202,38 @@ function afficherProduits(){
 
 
 
+
+
+
 function supprimerProduit(index){
 
+
     let produits = JSON.parse(localStorage.getItem("produits")) || [];
+
 
 
     produits.splice(index,1);
 
 
+
     localStorage.setItem("produits", JSON.stringify(produits));
+
 
 
     afficherProduits();
 
 
+
     alert("Pwodwi efase ✅");
+
 
 }
 
 
 
 
+
+
+checkLogin();
 
 afficherProduits();
